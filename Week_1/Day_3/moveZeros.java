972b84d78c9491ee77cf70059de6a2543b183647
abhinaykumar10 <24115cn068@glbitm.ac.in>
2026-06-03 22:58:55 +0530

    Create moveZeros.java

---
+++ Week_1/Day_3/moveZeros.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+       int i=0;
+       int j=1;
+       while(j<nums.length){
+        if(nums[i]==0&&nums[j]!=0){
+            int temp=nums[j];
+            nums[j]=nums[i];
+            nums[i]=temp;
+            i++;
+            j++;
+        }
+        else if(nums[i]==0 &&nums[j]==0){
+            j++;
+        }
+        else{
+            i++;
+            if(i==j){
+            j++;
+        }
+        }
+
+       }
+
+    
+}}
